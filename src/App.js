dd0df1e63a9f59c8204c4fe3085a3df3088ffde6
Hải Nguyễn <hainn@pmr.vn>
2020-10-02 11:01:21 +0700

    [REACT ESSENTIALS] Props

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import Card from './Card'
+import faker from 'faker'
 
 function App() {
+  const buttonsMarkup = (
+    <div>
+      <button className="button button2">YES</button>
+      <button className="button button3">NO</button>
+    </div>
+  )
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Card
+        avatar={faker.image.avatar()}
+        name={`${faker.name.firstName()} ${faker.name.firstName()}`}
+        title={faker.name.jobTitle()}
+      >
+        {buttonsMarkup}
+      </Card>
+      <Card
+        avatar={faker.image.avatar()}
+        name={`${faker.name.firstName()} ${faker.name.firstName()}`}
+        title={faker.name.jobTitle()}
+      >
+        {buttonsMarkup}
+      </Card>
+      <Card
+        avatar={faker.image.avatar()}
+        name={`${faker.name.firstName()} ${faker.name.firstName()}`}
+        title={faker.name.jobTitle()}
+      >
+        {buttonsMarkup}
+      </Card>
     </div>
   );
 }
